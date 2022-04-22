4de2f7127a26a819987852e18f269c7afd25ba99
yaodan <yaodan_sn@sina.com>
2022-04-22 15:59:18 +0800

    增加环境变量判断

--- producer-test/src/main/java/com/test/Application.java
+++ producer-test/src/main/java/com/test/Application.java
@@ -1,6 +1,9 @@
 package com.test;
 
 import com.summer.mq.spring.annotation.EnableSummerMq;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -17,9 +20,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableSummerMq(producerCorePoolSize = 10, producerMaximumPoolSize = 10)
-public class Application {
+@Slf4j
+public class Application implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("系统启动成功\n.....\nsuccess");
     }
 }
