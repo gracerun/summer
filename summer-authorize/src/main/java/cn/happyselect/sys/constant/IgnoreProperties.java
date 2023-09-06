42023565f6fe0c62923f86bac1a3ca803a22d0a9
yaodan <yaodan_sn@sina.com>
2023-09-06 13:46:54 +0800

    项目结构拆分

--- summer-authorize/src/main/java/cn/happyselect/sys/constant/IgnoreProperties.java
+++
@@ -1,9 +0,0 @@
-package cn.happyselect.sys.constant;
-
-/**
- * BeanUtil复制对象时设置忽略属性 IgnoreProperties
- */
-public interface IgnoreProperties {
-
-	String[] IGNORE = { "id", "optimistic", "createTime", "lastUpdateTime" };
-}
